12b5a8e7157f81391e4f66188a107c10579f88bc
Gabben <copyleft@sosus.org>
2023-04-14 18:18:16 +0500

    Add about page

--- src/nimidirectory.nim
+++ src/nimidirectory.nim
@@ -10,10 +10,14 @@ import nimidirectory/packages
 const
   PackagesURL = "https://raw.githubusercontent.com/nim-lang/packages/master/packages.json"
 
+let
+  buildTime = $now().utc
 
 proc homePage(): string =
-  let buildTime = $now().utc
   compileTemplateFile(getScriptDir() / "templates" / "home.nimja")
+
+proc aboutPage(): string =
+  compileTemplateFile(getScriptDir() / "templates" / "about.nimja")
 
 proc main() =
   let
@@ -26,6 +30,7 @@ proc main() =
     patchedJs = fmt"const packages = {packagesJson}{jsContent}"
   writeFile(getCurrentDir() / "public" / "js" / "app.js", patchedJs)
   writeFile(getCurrentDir() / "public" / "index.html", homePage())
+  writeFile(getCurrentDir() / "public" / "about.html", aboutPage())
 
 when isMainModule: 
   main()
